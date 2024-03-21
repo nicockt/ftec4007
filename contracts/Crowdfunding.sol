946e3eff3b32f9c85ed71116eed4e85e6dd3147e
nicockt <nico_ckt@yahoo.com.hk>
2024-03-22 01:47:52 +0800

    crowdfunding fix bug

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -25,45 +25,40 @@ contract Crowdfunding {
       uint256 endAt;
       uint256 startAt;
       bool ownerWithdrawn;
-      mapping(address => uint256) funders;
+      mapping(address => uint256) funders; // address -> amountDonated
       string projectName;
     }
 
     uint256 public projectCount = 0; // count total number of existing project
     mapping(uint256 => Project) public projects; // map project id to project
-   
 
-    event Launch(uint256 id, string projectName, address indexed owner, uint256 targetFund, uint256 startAt, uint256 endAt);
-    event Funded(address indexed _funder, uint256 _amount);
+    event Cancel(uint256 _id);
+    event Launch(uint256 _id, string _projectName, address indexed _owner, uint256 _targetFund, uint256 _startAt, uint256 _endAt);
+    event Fund(uint256 indexed _id, address indexed _funder, uint256 _amount);
     event OwnerWithdraw(uint256 _id);
     event Refund(uint256 _id , address indexed _funder, uint256 _amount);
-    event FunderWithdraw(address _funder, uint256 _amount);
-
-    constructor(uint256 _targetFund, uint256 _deadline){
-      deploymentTime = block.timestamp;
-      deadline = deploymentTime + _deadline;
-      targetFund = _targetFund;
-    }
+    event FunderWithdraw(uint256 indexed _id, address _funder, uint256 _amount);
 
     receive() external payable { }
 
     fallback() external payable { } 
 
-    function launch(string _projectName, uint256 _targetFund, uint256 _startAt, uint256 _endAt) external {
+    function launch(string memory _projectName, uint256 _targetFund, uint256 _startAt, uint256 _endAt) external {
         require(_startAt >= block.timestamp, "start at < now");
         require(_endAt > _startAt, "end at <= start at");
         require(_endAt <= block.timestamp + 730 days, "end at > max duration (2 years)");
+        require(_targetFund > 0, "targetFund <= 0");
 
         projectCount++;
-        projects[projectCount] = Project({
-          owner: msg.sender,
-          targetFund: _targetFund,
-          raisedFund: 0,
-          endAt: _endAt,
-          startAt: _startAt,
-          ownerWithdrawn: false,
-          projectName: _projectName,
-        });
+        Project storage newProject = projects[projectCount];
+        newProject.owner = msg.sender;
+        newProject.targetFund = _targetFund;
+        newProject.raisedFund = 0;
+        newProject.endAt = _endAt;
+        newProject.startAt = _startAt;
+        newProject.projectName = _projectName;
+        newProject.ownerWithdrawn = false;
+        
 
         emit Launch(projectCount, _projectName, msg.sender, _targetFund, _startAt, _endAt);
     }
@@ -84,11 +79,25 @@ contract Crowdfunding {
       return projects[projectId].endAt;
     }
 
-    //TODO: cancel project
+    function isSuccess(uint256 projectId)public view returns(bool){
+      return projects[projectId].raisedFund >= projects[projectId].targetFund;
+    }
+
+    // Cancel project before funding
+    function cancel(uint256 _id) external { "Project with the given ID does not exist.");
+        Project storage project = projects[_id];
+        require(project.startAt != 0, "Project not exists")
+        require(project.owner == msg.sender, "not creator");
+        require(block.timestamp < project.startAt, "started");
+
+        delete projects[_id];
+        emit Cancel(_id);
+    }
 
     // Owner get money after crowdfunding success
-    function withdrawOwner(uint256 _id) public {
+    function withdrawOwner(uint256 _id) external {
           Project storage project = projects[_id];
+        require(project.startAt != 0, "Project not exists")
           require(msg.sender == project.owner, "Not authorized!");
           require(project.raisedFund >= project.targetFund, "Not meet target");
           require(block.timestamp > project.endAt, "not ended");
@@ -102,8 +111,9 @@ contract Crowdfunding {
       }
 
     // Funder invest
-    function fund() public payable {
+    function fund(uint256 _id) external payable {
           Project storage project = projects[_id];
+        require(project.startAt != 0, "Project not exists")
           require(block.timestamp >= project.startAt, "Not started yet");
           require(block.timestamp <= project.endAt, "Ended");
 
@@ -113,30 +123,31 @@ contract Crowdfunding {
           project.raisedFund += msg.value;
           project.funders[msg.sender] += msg.value;
 
-          emit Funded(msg.sender, msg.value);
+          emit Fund(_id, msg.sender, msg.value);
       }
     
-    // Optional: Funder get back investment
-    // function withdrawFunder(uint256 _id) public {
-    //     Project storage project = projects[_id];
-    //     require(block.timestamp > project.endAt, "not end!");
-    //     require(funders[msg.sender]>0, "You are not a contributor");
+    // Optional: Funder get back investment during funding
+    function withdrawFunder(uint256 _id) external  {
+        Project storage project = projects[_id];
+        require(project.startAt != 0, "Project not exists")
+        require(block.timestamp <= project.endAt, "Ended");
+        require(project.funders[msg.sender] > 0, "You are not a contributor");
 
-    //     uint256 amountToSend = funders[msg.sender];
+        uint256 amountToSend = project.funders[msg.sender];
         
-    //     // Send ETH
-    //     (bool success,) = msg.sender.call{value: amountToSend}("");
-    //     require(success, "unable to send!");
-    //     payable(msg.sender).transfer(amountToSend);
-    //     raisedFund -= amountToSend;
-    //     funders[msg.sender]=0;
+        // Send money from contract to funder
+        (bool success,) = msg.sender.call{value: amountToSend}("");
+        require(success, "unable to send!");
+        project.raisedFund -= amountToSend;
+        project.funders[msg.sender]=0;
 
-    //     emit FunderWithdraw(msg.sender, amountToSend);
-    // }
+        emit FunderWithdraw(_id, msg.sender, amountToSend);
+    }
 
     // Fund refund after crowdfunding fails
-    function refund(uint256 _id) public {
+    function refund(uint256 _id) external {
           Project storage project = projects[_id];
+          require(project.startAt != 0, "Project not exists")
           require(block.timestamp > project.endAt, "not end!");
           require(project.funders[msg.sender]>0, "You are not a contributor");
           require(project.raisedFund < project.targetFund, "raisedFund > targetFund");
@@ -146,6 +157,7 @@ contract Crowdfunding {
           require(success, "unable to send!");
           project.raisedFund -= amountToSend;
           project.funders[msg.sender]=0;
+
           emit Refund(_id, msg.sender, amountToSend);
       }
 
