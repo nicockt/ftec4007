294d934178edd81df06cb8633c4b1ba5b409cc6e
nicockt <nico_ckt@yahoo.com.hk>
2024-03-21 16:49:06 +0800

    crowdfunding rewrite

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -8,95 +8,145 @@ interface IERC20 {
 
 
 contract Crowdfunding {
-    address public owner;
-    mapping(address => uint256) public funders; //mapping funder address to fund amount
-    uint256 public deadline;
-    uint256 public targetFund;
-    uint256 public raisedFund;
-    uint256 public deploymentTime;
-    uint256 public funded;
-    bool public ownerWithdrawn;
-    string public projectName;
+    // address public owner;
+    // mapping(address => uint256) public funders; //mapping funder address to fund amount
+    // uint256 public deadline;
+    // uint256 public targetFund;
+    // uint256 public raisedFund;
+    // uint256 public deploymentTime;
+    // uint256 public funded;
+    // bool public ownerWithdrawn;
+    // string public projectName;
 
-    enum Status{
-      Preparing,
-      Funding,
-      Failed,
-      Successful,
-      Closed
+    struct Project {
+      address owner; // creator of project
+      uint256 targetFund;
+      uint256 raisedFund;
+      uint256 endAt;
+      uint256 startAt;
+      bool ownerWithdrawn;
+      mapping(address => uint256) funders;
+      string projectName;
     }
-    Status public status;
 
-    IERC20 public immutable token;
+    uint256 public projectCount = 0; // count total number of existing project
+    mapping(uint256 => Project) public projects; // map project id to project
+   
 
+    event Launch(uint256 id, string projectName, address indexed owner, uint256 targetFund, uint256 startAt, uint256 endAt);
     event Funded(address indexed _funder, uint256 _amount);
-    event OwnerWithdraw(uint256 _amount);
+    event OwnerWithdraw(uint256 _id);
+    event Refund(uint256 _id , address indexed _funder, uint256 _amount);
     event FunderWithdraw(address _funder, uint256 _amount);
 
-    constructor(uint256 _targetFund, uint256 _deadline, address _token){
+    constructor(uint256 _targetFund, uint256 _deadline){
       deploymentTime = block.timestamp;
       deadline = deploymentTime + _deadline;
       targetFund = _targetFund;
-      status = status;
-      token = IERC20(_token);
     }
 
     receive() external payable { }
 
     fallback() external payable { } 
 
-    function createFund() external payable {
-      funders[msg.sender] = msg.value;
+    function launch(string _projectName, uint256 _targetFund, uint256 _startAt, uint256 _endAt) external {
+        require(_startAt >= block.timestamp, "start at < now");
+        require(_endAt > _startAt, "end at <= start at");
+        require(_endAt <= block.timestamp + 730 days, "end at > max duration (2 years)");
+
+        projectCount++;
+        projects[projectCount] = Project({
+          owner: msg.sender,
+          targetFund: _targetFund,
+          raisedFund: 0,
+          endAt: _endAt,
+          startAt: _startAt,
+          ownerWithdrawn: false,
+          projectName: _projectName,
+        });
+
+        emit Launch(projectCount, _projectName, msg.sender, _targetFund, _startAt, _endAt);
     }
 
-    function getMyFund() public view returns(uint256) {
-      return funders[msg.sender];
+    // function createFund() external payable {
+    //   funders[msg.sender] = msg.value;
+    // }
+
+    function getMyFund(uint256 projectId) public view returns(uint256) {
+      return projects[projectId].funders[msg.sender];
     }
 
-    function showDeadline()public view returns(uint256){
-      return deadline;
+    function getRaisedFund(uint256 projectId) public view returns (uint256) {
+      return projects[projectId].raisedFund;
     }
 
-    function getRasiedFund()public view returns(uint256){
-      return raisedFund;
+    function getDeadline(uint256 projectId)public view returns(uint256){
+      return projects[projectId].endAt;
     }
 
-    function withdrawOwner() public {
-          require(msg.sender == owner, "Not authorized!");
-          require(raisedFund >= targetFund, "Cannot withdraw!");
-          require(block.timestamp > deadline, "not ended");
-          require(!ownerWithdrawn, "Owner already withdrawn");
+    //TODO: cancel project
 
-          // Send ETH
-          (bool success,) = msg.sender.call{value: raisedFund}("");
-          require(success, "unable to send!");
-          ownerWithdrawn = true;
-          emit OwnerWithdraw(raisedFund);
+    // Owner get money after crowdfunding success
+    function withdrawOwner(uint256 _id) public {
+          Project storage project = projects[_id];
+          require(msg.sender == project.owner, "Not authorized!");
+          require(project.raisedFund >= project.targetFund, "Not meet target");
+          require(block.timestamp > project.endAt, "not ended");
+          require(!project.ownerWithdrawn, "Owner already withdrawn");
+
+          // Transfer fund to owner
+          (bool success,) = msg.sender.call{value: project.raisedFund}("");
+          require(success, "unable to transfer fund to owner");
+          project.ownerWithdrawn = true;
+          emit OwnerWithdraw(_id);
       }
 
+    // Funder invest
     function fund() public payable {
-          require(block.timestamp > deadline, "Funding is now disabled!");
-          raisedFund += msg.value;
-          funders[msg.sender] += msg.value;
-          // Give token to funder
-          token.transferFrom(msg.sender, address(this), msg.value);
+          Project storage project = projects[_id];
+          require(block.timestamp >= project.startAt, "Not started yet");
+          require(block.timestamp <= project.endAt, "Ended");
+
+          //TODO: set the value limit (eg: 100, 1000, 5000)
+          (bool success, ) = address(this).call{value: msg.value}("");
+          require(success, "Unable to transfer funds to the contract.");
+          project.raisedFund += msg.value;
+          project.funders[msg.sender] += msg.value;
+
           emit Funded(msg.sender, msg.value);
       }
+    
+    // Optional: Funder get back investment
+    // function withdrawFunder(uint256 _id) public {
+    //     Project storage project = projects[_id];
+    //     require(block.timestamp > project.endAt, "not end!");
+    //     require(funders[msg.sender]>0, "You are not a contributor");
 
-    function withdrawFunder() public {
-          require(block.timestamp > deadline, "Withdraw is now disabled!");
-          require(funders[msg.sender]>0, "You are not a contributor");
+    //     uint256 amountToSend = funders[msg.sender];
+        
+    //     // Send ETH
+    //     (bool success,) = msg.sender.call{value: amountToSend}("");
+    //     require(success, "unable to send!");
+    //     payable(msg.sender).transfer(amountToSend);
+    //     raisedFund -= amountToSend;
+    //     funders[msg.sender]=0;
 
-          uint256 amountToSend = funders[msg.sender];
-          
-          // Send ETH
+    //     emit FunderWithdraw(msg.sender, amountToSend);
+    // }
+
+    // Fund refund after crowdfunding fails
+    function refund(uint256 _id) public {
+          Project storage project = projects[_id];
+          require(block.timestamp > project.endAt, "not end!");
+          require(project.funders[msg.sender]>0, "You are not a contributor");
+          require(project.raisedFund < project.targetFund, "raisedFund > targetFund");
+
+          uint256 amountToSend = project.funders[msg.sender];
           (bool success,) = msg.sender.call{value: amountToSend}("");
           require(success, "unable to send!");
-          payable(msg.sender).transfer(amountToSend);
-          raisedFund -= amountToSend;
-          funders[msg.sender]=0;
-          token.transfer(msg.sender, amountToSend);
-          emit FunderWithdraw(msg.sender, amountToSend);
+          project.raisedFund -= amountToSend;
+          project.funders[msg.sender]=0;
+          emit Refund(_id, msg.sender, amountToSend);
       }
 
 }
