dd531fc1d7341249fb87840cf3f443cf564d5039
Nico Cheung <nico_ckt@yahoo.com.hk>
2024-03-25 17:09:37 +0800

    Rewrite NFT (bug: Counter)

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,43 +1,47 @@
 // SPDX-License-Identifier: MIT
-
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Votes.sol";
 
-contract NFT is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
-    constructor(address initialOwner)
-        ERC721("NFT", "Team10")
-        Ownable(initialOwner)
-    {}
+contract NFT is ERC721, ERC721Enumerable, ERC721Pausable, Ownable, ERC721Burnable, EIP712, ERC721Votes {
+    uint256 private _nextTokenId;
+    mapping(address => uint256[]) private _mintedTokens;
+    uint256 private _tokenIdCounter;
     uint256 public maxmint = 10;
     uint256 public currentMint;
-    uint256 private _nextTokenId;
-    using Counters for Counters.Counter;
 
-    Counters.Counter private _tokenIdCounter;
-    mapping(address => uint256[]) private _mintedTokens;
+    constructor(address initialOwner)
+        ERC721("NFT", "FTC")
+        Ownable(initialOwner)
+        EIP712("NFT", "1")
+    {}
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://team10.com/";
+    function pause() public onlyOwner {
+        _pause();
     }
 
-function safeMint(address to, uint256 numberOfTokens) public onlyOwner returns (uint256[] memory) {
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, uint256 numberOfTokens) public onlyOwner returns (uint256[] memory) {
         require(currentMint + numberOfTokens <= maxmint, "Max limit exceeded");
         
         uint256[] memory mintedTokenIds = new uint256[](numberOfTokens);
 
         for (uint256 i = 0; i < numberOfTokens; i++) {
-            uint256 tokenId = _tokenIdCounter.current();
+            uint256 tokenId = _tokenIdCounter;
             _safeMint(to, tokenId);
             mintedTokenIds[i] = tokenId;
             currentMint++;
-            _tokenIdCounter.increment();
+            _tokenIdCounter++;
         }
 
         _mintedTokens[to] = mintedTokenIds;
@@ -53,7 +57,7 @@ function safeMint(address to, uint256 numberOfTokens) public onlyOwner returns (
 
     function _update(address to, uint256 tokenId, address auth)
         internal
-        override(ERC721, ERC721Enumerable)
+        override(ERC721, ERC721Enumerable, ERC721Pausable, ERC721Votes)
         returns (address)
     {
         return super._update(to, tokenId, auth);
@@ -61,27 +65,17 @@ function safeMint(address to, uint256 numberOfTokens) public onlyOwner returns (
 
     function _increaseBalance(address account, uint128 value)
         internal
-        override(ERC721, ERC721Enumerable)
+        override(ERC721, ERC721Enumerable, ERC721Votes)
     {
         super._increaseBalance(account, value);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
     }
 
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        override(ERC721, ERC721Enumerable)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
     }
-
 }
